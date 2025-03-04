881b22810f9219e4c1a30e3100883d8d70bf421e
Jens Oechsler <joe@devzero.dk>
2025-03-04 22:24:59 +0100

    systemd-get-latest.sh

---
+++ scripts/systemd-get-latest.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+base=$(cd "$(dirname "$0")" || exit 1; pwd)
+rsync --delete -av ../upstream/systemd/ src/systemd/
+# git add src/systemd
+cd ../upstream/systemd || exit 1
+ucm=$(git log -1)
+cd "$base" || exit 1
+git commit --author="pos-bot <pos-bot@devzero.dk>" -m "Systemd sources updated from upstream
+---
+$ucm"
