6f795e266147ba63bd151fadb4540485c010c324
Jens Oechsler <joe@devzero.dk>
2025-03-06 20:09:11 +0100

    coreutils-get-latest.sh

---
+++ scripts/coreutils-get-latest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+base=$(cd "$(dirname "$0")/.." || exit 1; pwd)
+
+cd ../upstream/coreutils || exit 1
+rsync --delete --delete-excluded --exclude .idea --exclude .git -av ./ "${base}/src/coreutils/"
+ucm=$(git log -1)
+
+cd "$base" || exit 1
+git add -A src/coreutils
+git commit --author="pos-bot <pos-bot@devzero.dk>" -m "Coreutils sources updated from upstream
+---
+$ucm"
