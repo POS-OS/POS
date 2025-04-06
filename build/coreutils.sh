d085d2ab3022a75992900a90189522dbe1523321
Jens Oechsler <joe@devzero.dk>
2025-04-06 18:55:30 +0200

    build upd

--- build/coreutils.sh
+++ build/coreutils.sh
@@ -8,7 +8,19 @@ base=$(cd "$(dirname "$0")/../" || exit 1; pwd)
 out_dir=${base}/work/output
 
 mkdir -p "$out_dir/$component"
+mkdir -p "$out_dir/pos-$component"
 
 cd "${base}/src/${component}" || exit 1
 
-autoreconf -fiv
+autoreconf -fv
+automake -af
+
+./configure \
+            --prefix=/pos            \
+            --enable-no-install-program=kill,uptime \
+            --enable-install-program=arch \
+            --enable-systemd \
+            --with-tty-group \
+
+make CFLAGS="-fno-strict-aliasing -fpic -Dlint"
+#make DESTDIR="$out_dir/pos-$component" install
