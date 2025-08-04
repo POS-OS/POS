ba2d12c5d70521144c3288168cdd35d109d7f01a
Jens Oechsler <joe@devzero.dk>
2025-08-04 20:49:48 +0200

    tz-get-latest.sh upd

--- scripts/tz-get-latest.sh
+++ scripts/tz-get-latest.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 base=$(cd "$(dirname "$0")/.." || exit 1; pwd)
-component=procps
+component=tz
 
 cd "${base}/../upstream/${component}" || exit 1
-git checkout master
+git checkout main
 git pull
 rsync --delete --delete-excluded --exclude .idea --exclude .git -av ./ "${base}/src/${component}/"
 ucm=$(git log -1)
